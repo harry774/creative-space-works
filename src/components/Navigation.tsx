794aec3e5fd3f9500f4a31ae56e225eede8deb3a
harry774 <xylo.hardik@gmail.com>
2025-01-10 16:58:13 -0500

    Removed button for smaller screen

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -230,22 +230,6 @@ const Navigation = () => {
               </Link>
             ))}
           </div>
-          <button className="md:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
         </div>
       </div>
     </motion.nav>
