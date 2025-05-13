5cdc20d2d71844eb3aaace0194619ae0c08bbb74
harry774 <xylo.hardik@gmail.com>
2025-05-13 13:01:40 -0400

    Scroll reset to top on new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ScrollToTop from '@/components/ScrollToTop';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -17,6 +18,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Navigation />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<Services />} />
